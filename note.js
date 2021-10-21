92289e368fd0a979cca3ee05049fda487aec8899
sang <sang@gmail.com>
2021-10-21 23:07:37 +0700

    update reactjs_day1

---
+++ note.js
@@ -0,0 +1,37 @@
+// note arrow function
+// 1.arrow funtion not is context in object method
+// 2.arrow function not is constructor function
+
+// Module js
+// export/import
+// export:-default/-not default
+// import:import nameExport from linkfileExport
+
+// enhanced object literals
+// 1.Định nghĩa key:value cho object
+// let name='sang'
+// let age=21
+// const person={
+//     name,
+//     age
+// }
+// console.log(person)
+// 2.Định nghĩa method cho object
+// let name='sang'
+// let age=21
+// const person={
+//     name,
+//     age,
+//     getName(){
+//         return this.name
+//     }
+// }
+// console.log(person.getName())
+// 3.Định nghĩa key cho object dưới dạng biến
+// let fieldName = "name";
+// let fieldPrice = "price";
+// const course = {
+//   [fieldName]: "javacripts",
+//   [fieldPrice]: 1000,
+// };
+// console.log(course);
